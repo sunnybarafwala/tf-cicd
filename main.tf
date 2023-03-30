86db17afeb06d79e1857f9c2e76dbf661d80cdce
sunnybarafwala <122046318+sunnybarafwala@users.noreply.github.com>
2023-03-30 00:14:47 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -73,3 +73,58 @@ resource "azurerm_network_security_group" "NSG1" {
     environment = "Production"
   }
 }
+resource "azurerm_subnet" "Subnet1" {
+  name                 = "Subnet1"
+  resource_group_name  = azurerm_resource_group.rg1.name
+  virtual_network_name = azurerm_virtual_network.VNet1.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+resource "azurerm_network_interface" "Nic1" {
+  name                = "Nic1"
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.Subnet1.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.PublicIp1.id
+  }
+}
+resource "azurerm_virtual_machine" "VM1" {
+  name                  = "VM1"
+  location              = azurerm_resource_group.rg1.location
+  resource_group_name   = azurerm_resource_group.rg1.name
+  network_interface_ids = [azurerm_network_interface.Nic1.id]
+  vm_size               = "Standard_B1ms"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+ storage_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts-gen2"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "Sunny1"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+  }
+}
