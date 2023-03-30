bcb4f75f76cca4a87b520c4bac43fc394c0213d0
sunnybarafwala <122046318+sunnybarafwala@users.noreply.github.com>
2023-03-29 23:16:16 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,26 +30,15 @@ provider "azurerm" {
     features {}
 }
 
-
-
-
-provider "azuread" {
-    version         =   ">= 0.11"
-    client_id       =   var.client_id
-    client_secret   =   var.client_secret
-    tenant_id       =   var.tenant_id
-    alias           =   "ad"
-}
-
 resource "azurerm_resource_group" "rg1" {
   location = "East US"
   name     = "sunny-rg"
 }
 
-# Create virtual network
-resource "azurerm_virtual_network" "my_terraform_network" {
-  name                = "myVnet"
+resource "azurerm_virtual_network" "VNet1" {
+  name                = "VNet1"
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
   address_space       = ["10.0.0.0/16"]
-  location            = "East US"
-  resource_group_name = "sunny-rg"
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
 }
