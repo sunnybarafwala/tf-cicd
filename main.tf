98d23b966307a3e1d17291ef3ac8d44350d58b47
sunnybarafwala <122046318+sunnybarafwala@users.noreply.github.com>
2023-03-30 00:09:40 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -74,12 +74,3 @@ resource "azurerm_linux_web_app" "webapp" {
     minimum_tls_version = "1.2"
   }
 }
-
-#  Deploy code from a public GitHub repo
-resource "azurerm_app_service_source_control" "sourcecontrol" {
-  app_id             = azurerm_linux_web_app.webapp.id
-  repo_url           = "https://github.com/Azure-Samples/nodejs-docs-hello-world"
-  branch             = "master"
-  use_manual_integration = true
-  use_mercurial      = false
-}
